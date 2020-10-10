92d02f5b3ea23569a357be03d6bfb46da1591f27
youichiro <cinnamon416@gmail.com>
2020-10-11 04:44:35 +0900

    カラムの制約を追記

--- db/migrate/20201010194140_create_users.rb
+++ db/migrate/20201010194140_create_users.rb
@@ -1,12 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password_digest
-      t.integer :role
+      t.string :name, limit: 100, null: false
+      t.string :email, limit: 100, null: false
+      t.string :password_digest, null: false
+      t.integer :role, default: 0
 
       t.timestamps
+      t.index :email, unique: true
     end
   end
 end
