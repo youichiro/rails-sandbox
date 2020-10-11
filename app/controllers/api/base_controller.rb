3a70465c84f8ddd4da430e807b9b800fcc766252
youichiro <cinnamon416@gmail.com>
2020-10-12 00:00:02 +0900

    login_requiredをbefore_actionで実行

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,2 +1,15 @@
 class Api::BaseController < ApplicationController
+  before_action :login_required
+
+  def login_required
+    if !current_user
+      render json: { message: 'unauthorized' }, status: 401
+    end
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
