9a834fb3788338393c722f5524069415cb271bf5
youichiro <cinnamon416@gmail.com>
2020-10-12 00:01:21 +0900

    userとsessionのroutesを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :tasks, only: [:index, :show, :create, :update, :destroy]
+    resource :user, only: [:show, :create]
+    resource :session, only: [:create, :destroy]
   end
 end
