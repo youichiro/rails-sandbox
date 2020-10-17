6ef0918d97f17c9ca44317bf93735d0d7e5708d9
youichiro <cinnamon416@gmail.com>
2020-10-18 00:53:09 +0900

    モデルに関連付けを記述

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
+  belongs_to :user
   validates :name, presence: true, length: { maximum: 100 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :tasks
+
   enum role: { general: 0, admin: 1 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
